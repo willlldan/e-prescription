8b2fdd666817f4527ecc0aebb872b186186c5ca4
willlldan <173040071@mail.unpas.ac.id>
2022-05-26 16:20:54 +0700

    fix routing

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(url('/dashboard'));
 });
 
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
